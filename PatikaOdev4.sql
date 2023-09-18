6d4c5ed6f4db7a9e26e46c950bd19477a806dd9d
Baturay Acarturk <baturayacarturk@outlook.com>
2023-09-18 17:43:14 +0300

    patika4

---
+++ PatikaOdev4.sql
@@ -0,0 +1,5 @@
+--1. select distinct(replacement_cost) from film
+--2. select count(distinct(replacement_cost)) from film
+--3. select count(distinct(title)) from film where title like'T%' and rating='G'
+--4. select count(distinct(country)) from country where Length(country)=5
+--5. select count(distinct(city)) from city where city ilike '%r'
